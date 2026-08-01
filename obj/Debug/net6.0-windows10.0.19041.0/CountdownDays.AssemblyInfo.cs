5741dbb760f0eaab19290ee79522cc57606b1c47
CJKmkp <2564608840@qq.com>
2026-08-01 12:58:56 +0800

    improve:插件引用

--- obj/Debug/net6.0-windows10.0.19041.0/CountdownDays.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows10.0.19041.0/CountdownDays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CountdownDays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2896bb5f971e76647e7547bee0e237e6945187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b704e3ac7d7460ebace7a4ff262c271f8106d9ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CountdownDays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CountdownDays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
